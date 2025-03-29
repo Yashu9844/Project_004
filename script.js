d053954c5427996496db9b1ff622cbb9c0c0988f
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-03-29 22:59:12 +0530

    commit

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ gsap.from(".nlink", {
 
 Shery.textAnimate("#headings h1" /* Element to target.*/, {
     //Parameters are optional.
+    debug:true,
     style: 2,
     y:10,
     delay: 0.1,
@@ -58,6 +59,8 @@ gsap.from(".imgtext img",{
     ease:Expo,
 })    
 Shery.imageEffect(".bimg ",{
+  // debug:true,
+
     style:5,
     config:{"a":{"value":0.74,"range":[0,30]},"b":{"value":-0.9,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":2.0377474889461755},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.17,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":2.07,"range":[0,10]},"metaball":{"value":0.18,"range":[0,2]},"discard_threshold":{"value":0.61,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.38,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
     gooey:true
